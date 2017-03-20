b54b66d70f9c4898ab79351567c04483d222e84e
Nevelina Aleksandrova <nevelina@cottagelabs.com>
2017-03-20 16:25:37 +0000

    Moving geoblacklight
    Former-commit-id: 8aa7190950e93e88e06ab7760fbc802bb840ebde

--- geoblacklight/spec/features/layer_with_no_references_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-feature 'layer with no wms or iiif' do
-  scenario 'has map with bounding box' do
-    pending 'waiting on #201, need example metadata record'
-    fail
-  end
-end
