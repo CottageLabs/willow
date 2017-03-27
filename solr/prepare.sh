ae0ce28333dab20ea59b3d2ffd48095c06418b98
Martyn Whitwell <martyn.whitwell@gmail.com>
2017-03-27 18:37:32 +0100

    Now creates solr cores as required
    Also updating from Solr 6.2 to 6.4 (6.2 removed from docker source)
    
    
    Former-commit-id: 5fa0256ab87fa9ad6a8531032082c0d9cac45c01

--- solr/prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker-entrypoint.sh solr-precreate willow_development /opt/solr/server/solr/willow_config
-docker-entrypoint.sh solr-precreate geoblacklight_development /opt/solr/server/solr/geoblacklight_config
-
