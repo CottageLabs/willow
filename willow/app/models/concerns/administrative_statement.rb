607967412fdb68e17f180fded370a815cf11486e
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2017-05-09 00:28:54 +0100

    Inherit common methods

--- willow/app/models/concerns/administrative_statement.rb
+++ willow/app/models/concerns/administrative_statement.rb
@@ -14,16 +14,5 @@ class AdministrativeStatement < ActiveTriples::Resource
     super
   end
 
-  def final_parent
-    parent
-  end
-
-  def persisted?
-    !new_record?
-  end
-
-  def new_record?
-    id.start_with?('#')
-  end
-
+  include CommonMethods
 end
