7c7d9b7b179cdbc34407d145b415a1be8d4b3fb2
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2017-06-05 19:29:55 +0100

    Controlled vocabularies for DataCite dataset model added to local questioning authority

---
+++ willow/spec/services/personal_identifier_types_service_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe PersonalIdentifierTypesService do
+  before do
+    # Configure QA to use fixtures
+    qa_fixtures = { local_path: File.expand_path('../../../config/authorities', __FILE__) }
+    allow(Qa::Authorities::Local).to receive(:config).and_return(qa_fixtures)
+  end
+
+  describe "#select_all_options" do
+    it "returns all terms" do
+      expect(described_class.select_all_options).to include(
+        ["Orcid", 'http://purl.org/spar/datacite/orcid'])
+    end
+  end
+
+  describe "#label" do
+    it "resolves for ids of all terms" do
+      expect(described_class.label('http://purl.org/spar/datacite/isni')).to eq(
+        "International Standard Name Identifier scheme (isni)")
+    end
+  end
+end
