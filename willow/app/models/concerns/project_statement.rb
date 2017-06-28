28c2c554f75700661690248777cd1b0fbfacd00b
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2017-06-28 21:42:13 +0100

    Add grant number to project

--- willow/app/models/concerns/project_statement.rb
+++ willow/app/models/concerns/project_statement.rb
@@ -1,10 +1,14 @@
 require "./lib/vocabularies/rioxxterms"
+require "./lib/vocabularies/arpfo"
 class ProjectStatement < ActiveTriples::Resource
   include CommonMethods
 
-  # configure type: ::RDF::Vocab::MODS.adminMetadata
+  configure type: Arpfo.Project
   property :identifier, predicate: ::RDF::Vocab::Identifiers.local
+  property :title, predicate: ::RDF::Vocab::DC.title
   property :funder_name, predicate: RioxxTerms.funder_name
+  property :funder_id, predicate: RioxxTerms.funder_id
+  property :grant_number, predicate: Arpfo.grant_number
 
   ## Necessary to get AT to create hash URIs.
   def initialize(uri, parent)
