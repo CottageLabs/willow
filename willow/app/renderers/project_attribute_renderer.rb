ec55fe088a1b1db0702b31eec443c82f110759df
Anusha Ranganathan <anusha.ranganathan@ieee.org>
2017-07-02 22:26:18 +0100

    Add check for first array element for each field to not be blank

--- willow/app/renderers/project_attribute_renderer.rb
+++ willow/app/renderers/project_attribute_renderer.rb
@@ -5,19 +5,19 @@ class ProjectAttributeRenderer < CurationConcerns::Renderers::FacetedAttributeRe
     html = []
     value.each do |v|
       project = []
-      if v.include?('title') and not v['title'].blank?
+      if v.include?('title') and not v['title'].blank? and not v['title'][0].blank?
         project << "#{v['title'][0]}"
       end
-      if v.include?('identifier') and not v['identifier'].blank?
+      if v.include?('identifier') and not v['identifier'].blank? and not v['identifier'][0].blank?
         project << "identifier: #{v['identifier'][0]}"
       end
-      if v.include?('funder_name') and not v['funder_name'].blank?
+      if v.include?('funder_name') and not v['funder_name'].blank? and not v['funder_name'][0].blank?
         project << link_to(ERB::Util.h(v['funder_name'][0]), search_path(v['funder_name'][0]))
       end
-      if v.include?('funder_id') and not v['funder_id'].blank?
+      if v.include?('funder_id') and not v['funder_id'].blank? and not v['funder_id'][0].blank?
         project << "Funder Id: #{v['funder_id'][0]}"
       end
-      if v.include?('grant_number') and not v['grant_number'].blank?
+      if v.include?('grant_number') and not v['grant_number'].blank? and not v['grant_number'][0].blank?
         project << "Grant number: #{v['grant_number'][0]}"
       end
       html << project.join('<br>')
