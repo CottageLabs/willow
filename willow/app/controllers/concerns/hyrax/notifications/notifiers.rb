d3d29f551b053f8d3be5effa6b38ccdf8995794a
Anusha Ranganathan <anusha3@gmail.com>
2017-08-13 09:58:47 +0100

    Fixed issue with passing in wrong object in notifier

--- willow/app/controllers/concerns/hyrax/notifications/notifiers.rb
+++ willow/app/controllers/concerns/hyrax/notifications/notifiers.rb
@@ -5,17 +5,17 @@ module Hyrax
       included do
 
         def after_create_response
-          ActiveSupport::Notifications.instrument("create_work.hyrax", {curation_concern_type: self.class.curation_concern_type, object: hyrax})
+          ActiveSupport::Notifications.instrument("create_work.hyrax", {curation_concern_type: self.class.curation_concern_type, object: curation_concern})
           super
         end
 
         def after_update_response
-          ActiveSupport::Notifications.instrument("update_work.hyrax", {curation_concern_type: self.class.curation_concern_type, object: hyrax})
+          ActiveSupport::Notifications.instrument("update_work.hyrax", {curation_concern_type: self.class.curation_concern_type, object: curation_concern})
           super
         end
 
         def after_destroy_response(title)
-          ActiveSupport::Notifications.instrument("destroy_work.hyrax", {curation_concern_type: self.class.curation_concern_type, object: hyrax})
+          ActiveSupport::Notifications.instrument("destroy_work.hyrax", {curation_concern_type: self.class.curation_concern_type, object: curation_concern})
           super
         end
 
