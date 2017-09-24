035a3bc850e1c48e51dd3da228b99b03c556cae9
Nevelina Aleksandrova <nevelina@cottagelabs.com>
2017-09-24 11:38:26 +0100

    Institution name in ENV var

--- willow/app/helpers/branding_helper.rb
+++ willow/app/helpers/branding_helper.rb
@@ -1,9 +1,9 @@
 module BrandingHelper
-  # def institution_name
-  #   I18n.t('hyrax.institution_name', institution_name: ENV["INSTITUTION_NAME"])
-  # end
-  #
-  # def institution_name_full
-  #   I18n.t('hyrax.institution_name_full', institution_name_full: ENV["INSTITUTION_NAME_FULL"], default: institution_name)
-  # end
+  def institution_name
+    I18n.t('hyrax.institution_name', institution_name: ENV["INSTITUTION_NAME"])
+  end
+
+  def institution_name_full
+    I18n.t('hyrax.institution_name_full', institution_name_full: ENV["INSTITUTION_NAME_FULL"], default: institution_name)
+  end
 end
---
+++ willow/app/presenters/hyrax/permission_badge.rb
@@ -0,0 +1,41 @@
+module Hyrax
+  class PermissionBadge
+    include ActionView::Helpers::TagHelper
+
+    VISIBILITY_LABEL_CLASS = {
+        authenticated: "label-info",
+        embargo: "label-warning",
+        lease: "label-warning",
+        open: "label-success",
+        restricted: "label-danger"
+    }.freeze
+
+    # @param visibility [String] the current visibility
+    def initialize(visibility)
+      @visibility = visibility
+    end
+
+    # Draws a span tag with styles for a bootstrap label
+    def render
+      content_tag(:span, text, class: "label #{dom_label_class}")
+    end
+
+    private
+
+    def dom_label_class
+      VISIBILITY_LABEL_CLASS.fetch(@visibility.to_sym)
+    end
+
+    def text
+      if registered?
+        I18n.t('hyrax.institution_name', institution_name: ENV["INSTITUTION_NAME"])
+      else
+        I18n.t("hyrax.visibility.#{@visibility}.text")
+      end
+    end
+
+    def registered?
+      @visibility == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
+    end
+  end
+end
