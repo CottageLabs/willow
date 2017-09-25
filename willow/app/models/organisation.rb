46d46c5f267905c24f52cc6ecaacd36a2b23a8df
Anusha Ranganathan <anusha3@gmail.com>
2017-09-25 15:00:36 +0100

    Organisation model with basic metadata, identifiers and contact details

---
+++ willow/app/models/organisation.rb
@@ -0,0 +1,25 @@
+# Generated via
+#  `rails generate hyrax:work Book`
+class Organisation < ActiveFedora::Base
+  include ::BasicModelBehavior
+
+  self.indexer = AgentIndexer
+  # Change this to restrict which works can be added as a child.
+  # self.valid_child_concerns = []
+
+  self.human_readable_type = 'Organisation'
+
+  property :org_name, predicate: ::RDF::Vocab::VCARD.hasName do |index|
+    index.as :stored_searchable, :facetable
+  end
+  property :org_type, predicate: ::RDF::Vocab::DC.description do |index|
+    index.as :stored_searchable, :facetable
+  end
+  property :identifier_nested, predicate: ::RDF::Vocab::Identifiers.id, class_name: "ObjectIdentifier"
+  property :contact_nested, predicate: ::RDF::Vocab::DCAT.contactPoint, class_name: "ContactStatement"
+  include AgentNestedAttributes
+  # ToDo - associate with person
+  #   has_many :persons
+  # ToDo - associating with organisation role for datasets
+  #   has_many :datasets, through: :organisation_roles
+end
