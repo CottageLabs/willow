f7762daa3d5ac6e673b676341dc98ea80664691c
Nevelina Aleksandrova <nevelina@cottagelabs.com>
2017-09-26 15:10:11 +0100

    Provide default values for new env vars

--- willow/lib/willow/config.rb
+++ willow/lib/willow/config.rb
@@ -1,15 +1,15 @@
 module Willow
   class Config
      def self.institution_name
-       ENV["INSTITUTION_NAME"]
+       ENV["INSTITUTION_NAME"] || "Cottage Labs"
      end
 
      def self.institution_name_full
-       ENV["INSTITUTION_NAME_FULL"]
+       ENV["INSTITUTION_NAME_FULL"] || "Cottage Labs LLP"
      end
 
      def self.institution_logo
-       ENV["INSTITUTION_LOGO"]
+       ENV["INSTITUTION_LOGO"] || ""
      end
   end
 end
