265be052912a936616a4ce0d054e5de7a84dfd6b
Martyn Whitwell <martyn.whitwell@gmail.com>
2017-10-02 22:46:12 +0100

    One man went to mow

--- willow/spec/features/create_rdss_dataset_spec.rb
+++ willow/spec/features/create_rdss_dataset_spec.rb
@@ -6,15 +6,9 @@ include Warden::Test::Helpers
 # NOTE: If you generated more than one work, you have to set "js: true"
 RSpec.feature 'Create a RdssDataset', vcr: true, js: false do
   context 'a logged in user' do
-    let(:user_attributes) do
-      { email: 'test@example.com' }
-    end
-    let(:user) do
-      User.new(user_attributes) { |u| u.save(validate: false) }
-    end
+    let(:user) { create(:user) }
 
     before do
-      AdminSet.find_or_create_default_admin_set_id
       login_as user
     end
 
