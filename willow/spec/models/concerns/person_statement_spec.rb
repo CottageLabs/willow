255931bf9d895e2344d48a919e79ec4b5349481f
Anusha Ranganathan <anusha3@gmail.com>
2017-10-02 11:56:46 +0100

    Added identifier and uri fields

--- willow/spec/models/concerns/person_statement_spec.rb
+++ willow/spec/models/concerns/person_statement_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe PersonStatement, :vcr do
     Object.send(:remove_const, :ExampleWork)
   end
 
-  it 'creates a person active triple resource with an id, first name, last name, orcid and role' do
+  it 'creates a person active triple resource with an id and all properties' do
     @obj = ExampleWork.new
     @obj.attributes = {
       creator_nested_attributes: [
@@ -21,16 +21,21 @@ RSpec.describe PersonStatement, :vcr do
           name: 'Foo Bar',
           orcid: '0000-0000-0000-0000',
           affiliation: 'author affiliation',
-          role: 'Author'
+          role: 'Author',
+          identifier: '1234567',
+          uri: 'http://localhost/person/1234567'
         }
       ]
     }
     expect(@obj.creator_nested.first).to be_kind_of ActiveTriples::Resource
     expect(@obj.creator_nested.first.first_name).to eq ['Foo']
     expect(@obj.creator_nested.first.last_name).to eq ['Bar']
+    expect(@obj.creator_nested.first.name).to eq ['Foo Bar']
     expect(@obj.creator_nested.first.orcid).to eq ['0000-0000-0000-0000']
     expect(@obj.creator_nested.first.affiliation).to eq ['author affiliation']
     expect(@obj.creator_nested.first.role).to eq ['Author']
+    expect(@obj.creator_nested.first.identifier).to eq ['1234567']
+    expect(@obj.creator_nested.first.uri).to eq ['http://localhost/person/1234567']
   end
 
   it 'has the correct uri' do
@@ -42,7 +47,8 @@ RSpec.describe PersonStatement, :vcr do
           last_name: 'Bar',
           orcid: '0000-0000-0000-0000',
           affiliation: 'author affiliation',
-          role: 'Author'
+          role: 'Author',
+          identifier: '1234567',
         }
       ]
     }
