6f3f23feee695e923b3551c1fa36dbca925fb868
Martyn Whitwell <martyn.whitwell@gmail.com>
2017-10-04 17:58:47 +0100

    Work in progress
    -now syncing with S3
    -now reading RDSS vocabulary
    -need to pull in new data types

--- willow/lib/data_importer/importer.rb
+++ willow/lib/data_importer/importer.rb
@@ -1,57 +1,174 @@
 require 'aws-sdk'
+require 'fileutils'
 
 module DataImporter
   class Importer
 
-    def initialize(bucket: 'test-importer-data', region: 'eu-west-2')
-      s3 = Aws::S3::Client.new(region: region)
+    attr_reader :hash, :collection, :bucket, :import_dir, :filter
+    # http://testdata.researchdata.alpha.jisc.ac.uk.s3.eu-west-2.amazonaws.com/
+    def initialize(bucket: 'test-importer-data', region: 'eu-west-2', import_dir: 'tmp/imports', filter: nil,
+                   collection_id: nil, importing_user_email: nil)
+      @s3 = Aws::S3::Client.new(region: region)
+      @bucket = bucket
+      @import_dir = import_dir
+      @filter = filter
+      @collection_id = collection_id
+      @importing_user_email = importing_user_email
+      @collection = []
+      @hash = {}
 
-      s3.list_objects_v2(bucket: bucket).each do |response|
-        # puts response
+      @vocabs = VocabularyImporter.new('seed/jisc-rdss-datamodels')
+    end
 
 
-        tree = {}
 
-        # This code intends to iterate through an AWS bucket to construct a hash-tree
-        # /folder1/folder2/folder3/fileA
-        # /folder1/folder2/folder3/fileB
-        # /folder1/folder2/fileC
-        # should result in:
-        # { folder1: {folder2: {folder3: { fileA: {}, fileB: {} }, fileC: {} } } }
-        # However it is still work in progress!
+    def sync_with_s3
+      puts "Synchronising with S3"
+      unless File.directory?(@import_dir)
+        FileUtils.mkdir_p(@import_dir)
+      end
 
+      @s3.list_objects_v2(bucket: @bucket).each do |response|
         response.contents.each do |item|
-          # puts "item"
-          # puts item
-          puts item.key
 
-          split_into_hash(item.key, tree)
+          # we are only interested in files
+          unless item.key.ends_with?('/')
 
-          # puts item.class
+            if @filter.nil? || @filter.present? && item.key.starts_with?(@filter)
+
+              # Download the S3 files, unless they are already downloaded
+              filename = File.join(@import_dir, item.key)
+              dirname = File.dirname(filename)
+              # unless the file is already on disk and the sizes match, copy the file to disk
+              if File.exist?(filename) && File.size(filename) == item.size
+                puts "Skipping download of #{item.key}"
+              else
+                puts "Downloading #{item.key} (#{item.size})"
+                unless File.directory?(dirname)
+                  FileUtils.mkdir_p(dirname)
+                end
+                @s3.get_object(response_target: filename, bucket: @bucket, key: item.key)
+              end
+
+              hash[dirname] ||= {
+                  id: File.dirname(item.key),
+                  metadata: nil,
+                  files: []
+              }
+
+              if File.basename(filename) == 'metadata.json'
+                hash[dirname][:metadata] = filename
+              else
+                hash[dirname][:files].append(filename)
+              end
+
+            else
+              puts "Ignoring #{item.key} (filter: #{@filter})"
+            end
+          end
+
         end
-        
-        puts tree
+      end
+
+      @collection = @hash.map{|key,value| value}
+      self
+    end
 
 
-        # puts response.contents.map(&:key)
+    def import_into_willow
+      puts "Importing into Willow"
+      @collection.each do |item|
+        metadata = JSON.parse(File.read(item[:metadata]))
+
+
+
+
+        work = Book.where(id: item[:id]).first || Book.new(id: item[:id])
+        work.title = [item['objectTitle']]
+
+
+
+
+
+        puts "ID: #{item[:id]}"
+        puts "RESOURCE TYPE: #{@vocabs.vocabularies["resourceType"][metadata["objectResourceType"]]}"
+
+        metadata["objectIdentifier"].each do |i|
+          puts "IDENTIFIER: #{@vocabs.vocabularies["identifierType"][i["identifierType"]]} : #{i["identifierValue"]}"
+        end
+
+        metadata["objectPersonRole"].each do |i|
+          puts "PERSON: #{@vocabs.vocabularies["personRole"][i["role"]]} : #{i["person"]["personIdentifierValue"]} : #{@vocabs.vocabularies["personIdentifierType"][i['person']['personIdentifierType']]} "
+        end
+
+
+
+
+        # #puts metadata
         #
-        # puts response.contents.each.map do |c|
-        #   puts c.inspect
-        # end
+        # puts "TESST1"
+        # puts metadata["objectResourceType"]
+        # # objectResourceType is resourceType
+        # puts @vocabs.vocabularies["resourceType"][metadata["objectResourceType"]]
+
       end
 
+      self
     end
 
-    def split_into_hash(path, hash={})
+  end
+end
 
-      outer, inner = path.split("/", 2)
-      if inner
-        hash[outer] = (hash[outer] || {}).merge(split_into_hash(inner))
-      else
-        hash[outer] = (hash[outer] || {}).merge({})
-      end
-      hash
-    end
+
+# puts JSON.pretty_generate(hash)
+
+
+    # def parse_entry(item, collection, s3)
+    #   # ignore folders, just focus on files
+    #   if !item.key.ends_with?('/')
+    #     dirs, _, filename = item.key.rpartition('/')
+    #     collection[dirs] ||= {}
+    #
+    #     if filename == 'metadata.json'
+    #       collection[dirs][:metadata] = JSON.parse('{}')
+    #
+    #       #s3.get_object(item)
+    #
+    #       resp = s3.get_object(bucket: 'test-importer-data', key: item.key)
+    #       puts resp.body.read
+    #
+    #
+    #     end
+    #
+    #
+    #
+    #
+    #   end
+    #
+    #   # dirs = key.split("/")
+    #   # dirs.each do |dir|
+    #   #   hash[dir]
+    #   # end
+    #
+    #
+    # end
+
+    # def parse_dirs(dirs)
+    #
+    # end
+    #
+    #
+    #
+    # def split_into_hash(path, hash={})
+    #
+    #   outer, inner = path.split("/", 2)
+    #   if inner
+    #     hash[outer] = (hash[outer] || {}).merge(split_into_hash(inner))
+    #   else
+    #     hash[outer] = (hash[outer] || {}).merge({})
+    #   end
+    #   hash
+    # end
 
 
 
@@ -70,7 +187,5 @@ module DataImporter
 
 
 
-    end
+    #end
 
-  end
-end
---
+++ willow/seed/jisc-rdss-datamodels/README.txt
@@ -0,0 +1,3 @@
+The data models in this folder are here temporarily and will be replaced with an integrated message API.
+
+See https://github.com/JiscRDSS/taxonomyschema/tree/develop/datamodels
