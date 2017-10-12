2af57b0697429c26fcc36c9fd240132f39bf42d7
Anusha Ranganathan <anusha3@gmail.com>
2017-10-12 06:20:44 +0100

    Fixed terms in tests

--- willow/spec/services/rdss_identifier_types_service_spec.rb
+++ willow/spec/services/rdss_identifier_types_service_spec.rb
@@ -10,7 +10,7 @@ describe RdssIdentifierTypesService do
   describe "#select_all_options" do
     it "returns all terms" do
       expect(described_class.select_all_options).to include(
-        ["PUIDPREMIS", 'PUIDPREMIS'])
+        ["PUID", 'PUID'])
     end
   end
 
