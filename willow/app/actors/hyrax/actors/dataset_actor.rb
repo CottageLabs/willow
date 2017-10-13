504b34c6f298725579e872178f5031757e93e7f7
Anusha Ranganathan <anusha3@gmail.com>
2017-10-13 23:36:26 +0100

    Deleted dataset files

--- willow/app/actors/hyrax/actors/dataset_actor.rb
+++
@@ -1,8 +0,0 @@
-# Generated via
-#  `rails generate hyrax:work Dataset`
-module Hyrax
-  module Actors
-    class DatasetActor < Hyrax::Actors::BaseActor
-    end
-  end
-end
--- willow/app/presenters/hyrax/dataset_presenter.rb
+++
@@ -1,8 +0,0 @@
-# Generated via
-#  `rails generate hyrax:work Dataset`
-module Hyrax
-  class DatasetPresenter < Hyrax::WorkShowPresenter
-    delegate :doi, :other_title, :creator_nested, :date, :license_nested,
-      :relation, :subject_nested, :admin_metadata, :identifier_nested, to: :solr_document
-  end
-end
