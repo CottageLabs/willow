bc6c1bbbeaf38c025ea9d2ed1e397c6df1c61c62
Finlay McCourt <finlay.mccourt@digirati.com>
2017-12-12 17:09:45 +0000

    Removing basic_metadata fields from form.

--- willow/app/forms/hyrax/rdss_cdm_form.rb
+++ willow/app/forms/hyrax/rdss_cdm_form.rb
@@ -3,8 +3,32 @@
 module Hyrax
   class RdssCdmForm < Hyrax::Forms::WorkForm
     self.model_class = ::RdssCdm
-    self.terms += [
+
+    #
+    self.terms -= [
+      # These fields are not present in the RDSS CDM model, but 
+      :label, 
+      :relative_path, 
+      :import_url, 
       :resource_type,
+      :creator, 
+      :contributor, 
+      :description, 
+      :keyword, 
+      :license,
+      :rights_statement,
+      :publisher, 
+      :date_created, 
+      :subject, 
+      :language, 
+      :identifier, 
+      :based_near, 
+      :related_url, 
+      :bibliographic_citation, 
+      :source
+    ]
+
+    self.terms += [
       :title, 
       :object_description  
     ]
