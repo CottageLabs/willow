dadabf332541e35cec75df4aed58056b844826bf
Finlay McCourt <finlay.mccourt@digirati.com>
2017-12-18 11:21:59 +0000

    Updating indexer with all fields.

--- willow/app/indexers/rdss_cdm_indexer.rb
+++ willow/app/indexers/rdss_cdm_indexer.rb
@@ -3,12 +3,16 @@
 class RdssCdmIndexer < Hyrax::WorkIndexer
   # This indexes the default metadata. You can remove it if you want to
   # provide your own metadata and indexing.
-  #include Hyrax::IndexesBasicMetadata
+  include Hyrax::IndexesBasicMetadata
+  
+  # Overwriting these properties from Hyrax::IndexesBasicMetadata
+  self.stored_and_facetable_fields = %i[object_keywords object_category]
+  self.stored_fields = %i[title object_description object_version]
+  self.symbol_fields = %i[object_uuid]
 
-  def generate_solr_document
-    super.tap do |solr_doc|
-      solr_doc[Solrizer.solr_name('title', :stored_searchable)] = object.title
-      solr_doc[Solrizer.solr_name('object_description', :stored_searchable)] = object.object_description
-    end
-  end
+  #def generate_solr_document
+  #  super.tap do |solr_doc|
+  #    solr_doc[Solrizer.solr_name('title', :stored_searchable)] = object.title
+  #  end
+  #end
 end
