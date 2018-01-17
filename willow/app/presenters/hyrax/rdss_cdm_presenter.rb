259c42109bfbe6239dd54f8dc59c380eb8e8e907
Daniel Berzon <daniel@ocasta.com>
2018-01-17 12:44:04 +0000

    Rendering object_dates on the RdssCdm show page

--- willow/app/presenters/hyrax/rdss_cdm_presenter.rb
+++ willow/app/presenters/hyrax/rdss_cdm_presenter.rb
@@ -2,6 +2,13 @@
 #  `rails generate hyrax:work RdssCdm`
 module Hyrax
   class RdssCdmPresenter < Hyrax::WorkShowPresenter
-    delegate :title, :object_description, :object_keywords, :object_category, :object_resource_type, :object_value, to: :solr_document
+    delegate :title,
+             :object_description,
+             :object_keywords,
+             :object_category,
+             :object_resource_type,
+             :object_value,
+             :object_dates,
+             to: :solr_document
   end
 end
---
+++ willow/app/renderers/object_dates_attribute_renderer.rb
@@ -0,0 +1,29 @@
+class ObjectDatesAttributeRenderer < Hyrax::Renderers::AttributeRenderer
+  private
+  # translate the json string for object date into a table of displayable dates
+  # parse the json, and create a table where each row has a cell with the date type and the date value
+  def attribute_value_to_html(value)
+    value = JSON.parse(value)
+    if not value.kind_of?(Array)
+      value = [value]
+    end
+    html = '<table class="table"><tbodby>'
+    value.each do |v|
+      label = ''
+      val = ''
+      if(type = v['date_type'])
+        label = RdssDateTypesService.label(type)
+      end
+      if(date = v['date_value'])
+        begin
+          val = Date.parse(date).to_formatted_s(:standard)
+        rescue
+          val = date
+        end
+      end
+      html += "<tr><th>#{label}</th><td>#{val}</td><tr>"
+    end
+    html += '</tbody></table>'
+    %(#{html})
+  end
+end
