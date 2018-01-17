8f18c0e23f00453d7d0d435ff00e4075448cba14
Daniel Berzon <daniel@ocasta.com>
2018-01-17 10:11:30 +0000

    Changes to the indexer to index the new object_dates association

--- willow/app/indexers/rdss_cdm_indexer.rb
+++ willow/app/indexers/rdss_cdm_indexer.rb
@@ -5,6 +5,18 @@ class RdssCdmIndexer < Hyrax::WorkIndexer
     super.tap do |solr_doc|
       # Enter any manual indexing code here
       # if possible, indexing should be specified within the model
+
+      # index a displayable version of the object date
+      solr_doc[Solrizer.solr_name('object_dates', :displayable)] = object.object_dates.to_json
+      
+      # for each object date, index a value for the specific date type to allow sorting by the date type
+      # eg object_date_approved
+      object.object_dates.each do |d|
+        label = RdssDateTypesService.label(d.date_type) rescue nil
+        if label
+          solr_doc[Solrizer.solr_name("object_dates_#{label.downcase}", :stored_sortable)] = d.date_value
+        end
+      end
     end
   end
 end
