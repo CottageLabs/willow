ed37e900e72ad7de5acd8eb136b2c2c5efaf00ad
Paul McKibbin <pmckibbin@gmail.com>
2018-01-22 15:13:19 +0000

    Revert to the model based tests

--- willow/spec/models/cdm/date_spec.rb
+++ willow/spec/models/cdm/date_spec.rb
@@ -1,48 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Cdm::Date, :vcr do
-  before :all do
-    class ExampleWork < ActiveFedora::Base
-      property :object_date, predicate: ::RDF::Vocab::DC11.date, class_name: "Cdm::Date"
-      accepts_nested_attributes_for :object_date
+  # since this is a model, we just check it can build the correct fields
+  describe 'date_type' do
+    it 'has a single valued date_type' do
+      @obj = build(:cdm_date, date_type: 'created')
+      expect(@obj.date_type).to be_kind_of String
+      expect(@obj.date_type).to eq 'created'
     end
-  end
 
-  after :all do
-    Object.send(:remove_const, :ExampleWork)
-  end
-
-  let(:date_type) { 'copyrighted' }
-  let(:date_value) { '10/02/2018' }
-  let(:attributes) do
-    {
-      object_date_attributes: [
-        {
-          date_type: date_type,
-          date_value: date_value
-        }
-      ]
-    }
-  end
-
-  before :each do
-    @obj = ExampleWork.new
-    @obj.attributes = attributes
-  end
-
-  it 'creates an ObjectDate element of kind ActiveTriple::Resource' do
-    expect(@obj.object_date[0]).to be_kind_of ActiveTriples::Resource
-  end
-
-  it 'creates the correct date type property' do
-    expect(@obj.object_date[0].date_type).to eq([date_type])
-  end
-
-  it 'creates the correct date value property' do
-    expect(@obj.object_date[0].date_value).to eq([date_value])
-  end
-
-  it 'has the correct uri' do
-    expect(@obj.object_date[0].id).to include('#object_date')
+    it 'has a single valued date_value' do
+      @obj = build(:cdm_date, date_value: '10/18/2018')
+      expect(@obj.date_value).to be_kind_of String
+      expect(@obj.date_value).to eq '10/18/2018'
+    end
   end
 end
