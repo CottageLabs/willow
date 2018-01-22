44b15d7def595faa1a01418ee54b489d0a2ff3e0
Paul McKibbin <pmckibbin@gmail.com>
2018-01-22 14:47:05 +0000

    Tidy up of add and remove buttons

--- willow/app/models/rdss_cdm.rb
+++ willow/app/models/rdss_cdm.rb
@@ -23,7 +23,7 @@ class RdssCdm < ActiveFedora::Base
   #property :object_rights
 
   # Object date nested property
-  property :object_date, predicate: ::RDF::Vocab::DC.date, class_name: "Cdm::Date"
+  # property :object_date, predicate: ::RDF::Vocab::DC.date, class_name: "Cdm::Date"
 
   property :object_keywords, predicate: ::RDF::Vocab::DC11.relation do |index|
     index.as :stored_searchable, :facetable
@@ -43,10 +43,10 @@ class RdssCdm < ActiveFedora::Base
   #property :object_file
 
   # object_date nested relationship
-  # has_many :object_dates, class_name: 'Cdm::Date'
+  has_many :object_dates, class_name: 'Cdm::Date'
 
   # Accepts nested attributes declarations need to go after the property declarations, as they close off the model
-  accepts_nested_attributes_for :object_dates, reject_if: :object_dates_blank?, allow_destroy: true
+  accepts_nested_attributes_for :object_dates, reject_if: :object_date_blank?, allow_destroy: true
 
   
   def self.multiple?(field)
@@ -91,6 +91,4 @@ class RdssCdm < ActiveFedora::Base
   def object_dates_blank? attributes
     attributes[:date_value].blank?
   end
-
-
 end
