6d0288bbe3b5c8de583aac93af406d3c96f996c4
Paul McKibbin <pmckibbin@gmail.com>
2018-01-23 09:38:57 +0000

    Fix conflicts with 170

--- willow/app/models/concerns/rdss_cdm_extensions.rb
+++ willow/app/models/concerns/rdss_cdm_extensions.rb
@@ -2,6 +2,6 @@ module Concerns
   module RdssCdmExtensions
     include ::Solr::Concerns::IndexTypes
 
-    stored_searchable :title, :object_description, :object_keywords, :object_category
+    stored_searchable :title, :object_description, :object_keywords, :object_category, :object_resource_type, :object_value
   end
 end
