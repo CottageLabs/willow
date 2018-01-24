e482613a9d0b00c5ee06d148f0558850e2abe3bd
Paul McKibbin <pmckibbin@gmail.com>
2018-01-24 13:59:38 +0000

    Add :object_dates back in after overenthusiastic merge.

--- willow/app/models/concerns/rdss_cdm_extensions.rb
+++ willow/app/models/concerns/rdss_cdm_extensions.rb
@@ -3,5 +3,6 @@ module Concerns
     include ::Solr::Concerns::IndexTypes
 
     stored_searchable :title, :object_description, :object_keywords, :object_category, :object_resource_type, :object_value
+    displayable :object_dates
   end
 end
