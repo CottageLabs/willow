6e92d00cb4d0c02b298953c0235db38080a192e4
Daniel Berzon <daniel@ocasta.com>
2018-01-26 12:22:35 +0000

    Solr indexing of the object rights

--- willow/app/indexers/rdss_cdm_indexer.rb
+++ willow/app/indexers/rdss_cdm_indexer.rb
@@ -29,6 +29,23 @@ class RdssCdmIndexer < Hyrax::WorkIndexer
         solr_doc[Solrizer.solr_name("object_person_roles_#{r.role_type}", :stored_sortable)] = true
       end
 
+      # object rights
+      # At the moment we have a has_one relationship between Object and Object rights. As such it makes more sense to 
+      # index the object_rights fields as direct fields on the solr document
+      rights = object.object_rights.first
+      if rights
+        solr_doc[Solrizer.solr_name('object_rights_license', :stored_searchable)] = rights.license
+        solr_doc[Solrizer.solr_name('object_rights_rights_statement', :stored_searchable)] = rights.rights_statement
+        solr_doc[Solrizer.solr_name('object_rights_rights_holder', :stored_searchable)] = rights.rights_holder
+        # nested accesses
+        # we need to manually remove any that are marked for destruction
+        accesses = rights.accesses.reject(&:marked_for_destruction?)
+        # make access types searchable
+        solr_doc[Solrizer.solr_name('object_rights_access_type', :stored_searchable)] = accesses.map(&:access_type)
+        # for display include the entire nested access as json
+        solr_doc[Solrizer.solr_name('object_rights_accesses', :displayable)] = accesses.to_json
+      end
+
     end
   end
 end
