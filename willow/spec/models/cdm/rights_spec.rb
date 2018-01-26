c3deb3e30f85f409240d98466508b494394be9f2
Daniel Berzon <daniel@ocasta.com>
2018-01-26 14:08:38 +0000

    Rights spec, adding nested attributes test for accesses

--- willow/spec/models/cdm/rights_spec.rb
+++ willow/spec/models/cdm/rights_spec.rb
@@ -26,5 +26,33 @@ RSpec.describe Cdm::Rights do
       expect(@obj.license).to eq ['http://creativecommons.org/licenses/by/3.0/us/']
     end
   end
+
+  describe 'nested attributes for accesses' do
+    it 'accepts accesses attributes' do
+      @obj = build(:cdm_rights, accesses_attributes: [{ access_type: 'controlled', access_statement: 'Statement 1' }])
+      expect(@obj.accesses.first).to be_kind_of ActiveFedora::Base
+      expect(@obj.accesses.first.access_type).to eq 'controlled'
+      expect(@obj.accesses.first.access_statement).to eq 'Statement 1'
+    end
+
+    it 'rejects accesses attributes if type is blank' do
+      @obj = build(:cdm_rights, accesses_attributes: [
+                                                  {
+                                                    access_statement: 'statement 1',
+                                                    access_type: 'controlled'
+                                                  },
+                                                  {
+                                                    access_type: 'controlled'
+                                                  },
+                                                  {
+                                                    access_statement: 'statement 2'
+                                                  },
+                                                  {
+                                                    access_statement: ''
+                                                  }
+                                                ])
+      expect(@obj.accesses.size).to eq(2)
+    end
+  end
     
 end
