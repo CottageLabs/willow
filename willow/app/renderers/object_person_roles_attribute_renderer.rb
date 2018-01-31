8db4a965505e61d2828ef19601e8456d3916046f
Paul McKibbin <pmckibbin@gmail.com>
2018-01-31 14:28:34 +0000

    Post review changes and refactor to allow quick switch between tables and css tables.

--- willow/app/renderers/object_person_roles_attribute_renderer.rb
+++ willow/app/renderers/object_person_roles_attribute_renderer.rb
@@ -1,16 +1,24 @@
 class ObjectPersonRolesAttributeRenderer < Hyrax::Renderers::AttributeRenderer
   private
+  include Concerns::CssTableRenderer
+
   def i18n_prefix
     'rdss.person_roles.'
   end
 
+  def role_types(value, converter=::Cdm::Json::ObjectPersonRoles)
+    converter.new(value).map(&:role_type)
+  end
+
   def attribute_value_to_html(value)
-    role_types=::Cdm::Json::ObjectPersonRoles.new(value).map(&:role_type)
-    html = '<div class="table"><div class="tbody">'
-    role_types.each do |v|
-      html << '<div class="tr"><div class="th">'+I18n.t(i18n_prefix + v.to_s)+'</div></div>'
-    end
-    html << '</div></div>'
-    %(#{html})
+    table {
+      thead {
+        role_types(value).map do |v|
+          row {
+            header { I18n.t(i18n_prefix + v.to_s) }
+          }
+        end.join
+      }
+    }
   end
 end
