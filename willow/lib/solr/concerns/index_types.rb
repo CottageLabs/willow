933ea99b0ec1863d01a3ddc00865215aeec315b4
Paul McKibbin <pmckibbin@gmail.com>
2018-02-07 13:58:33 +0000

    Tighten up the .to_s handling in the IndexTypes.

--- willow/lib/solr/concerns/index_types.rb
+++ willow/lib/solr/concerns/index_types.rb
@@ -11,14 +11,14 @@ module Solr
             names.each do |name|
               #use the instance solr_name definition defined in this file unless overridden
               define_method name do |*options|
-                solr_value(name.to_s, index_type, *options)
+                solr_value(name, index_type, *options)
               end
             end
           end
 
           #Uses the class solr_name definition as defined in Hydra::Controller::ControllerBehaviour and possibly others
           define_singleton_method(index_type.to_s + '_name') do |name, *options|
-            solr_name(name.to_s, index_type, *options)
+            solr_name(name, index_type, *options)
           end
         end
       end
