01f1b8b864f50965d3dcbdb401c943427bfd0cae
Finlay McCourt <finlay.mccourt@digirati.com>
2018-02-23 15:04:27 +0000

    Adding in check that work is approved for update and destroy events.
    This is to avoid messages being generated for works that are being
    updated prior to approval. Equally we do not want to generate
    destroy messages for works that have not been previously published to
    the RDSS. When considering workflows in the future we should be careful
    not to have works deactivated before a destroy message is generated.

--- willow/lib/rdss/messaging/actors/message_publisher_actor.rb
+++ willow/lib/rdss/messaging/actors/message_publisher_actor.rb
@@ -6,13 +6,23 @@ module Rdss
         
         def update(env)
           next_actor.update(env)
-          broadcast(:work_update_minor, env.curation_concern)
+          if work_approved?(env)
+            broadcast(:work_update_minor, env.curation_concern)
+          end
         end
 
         def destroy(env)
           next_actor.destroy(env)
-          broadcast(:work_destroy, env.curation_concern)
+          if work_approved?(env)
+            broadcast(:work_destroy, env.curation_concern)
+          end
         end
+
+        private 
+          def work_approved?(env)
+            env.curation_concern.state == Vocab::FedoraResourceStatus.active
+          end
+
       end
     end
   end
