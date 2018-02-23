8bc450d67c3fe8a179cf1d943aa71e2701897d20
hwjisc <howard.williams@jisc.ac,uk>
2018-02-23 11:21:06 +0000

    basic working version with logging

--- willow/app/actors/hyrax/actors/rdss_cdm_object_versioning_actor.rb
+++ willow/app/actors/hyrax/actors/rdss_cdm_object_versioning_actor.rb
@@ -3,21 +3,23 @@ module Hyrax
     class RdssCdmObjectVersioningActor < AbstractActor
 
       def create(env)
+        env.attributes[:object_version] = 1
         log_details(env, __method__)
-
         next_actor.create(env)
       end
 
       def update(env)
+        if significant_fields_changed(env)
+          increment_object_version(env)
+        end
         log_details(env, __method__)
-
         next_actor.update(env)
       end
 
       private
 
       def log_details(env, from)
-        Rails.logger.info "RdssCdmObjectVersioningActor #{ from }:
+        Rails.logger.info "RdssCdmObjectVersioningActor #{from}:
         {
           curation_concern.title: #{env.curation_concern.title},
           attribute.title: #{env.attributes[:title]},
@@ -32,6 +34,10 @@ module Hyrax
           env.attributes[:uploaded_files].count.positive?
       end
 
+      def increment_object_version(env)
+        env.attributes[:object_version] =
+            (env.attributes[:object_version].to_i + 1).to_s
+      end
     end
   end
 end
