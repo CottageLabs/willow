06a1eadfd7fa752400d4b924716c7cb2901a1120
hwjisc <howard.williams@jisc.ac,uk>
2018-02-23 15:34:47 +0000

    object version increments on update only if approved

--- willow/app/actors/hyrax/actors/rdss_cdm_object_versioning_actor.rb
+++ willow/app/actors/hyrax/actors/rdss_cdm_object_versioning_actor.rb
@@ -3,40 +3,54 @@ module Hyrax
     class RdssCdmObjectVersioningActor < AbstractActor
 
       def create(env)
-        env.attributes[:object_version] = 1
-        log_details(env, __method__)
+        perform_object_versioning(env)
         next_actor.create(env)
       end
 
       def update(env)
-        if significant_fields_changed(env)
-          increment_object_version(env)
-        end
-        log_details(env, __method__)
+        perform_object_versioning(env)
         next_actor.update(env)
       end
 
       private
 
-      def log_details(env, from)
-        Rails.logger.info "RdssCdmObjectVersioningActor #{from}:
-        {
-          curation_concern.title: #{env.curation_concern.title},
-          attribute.title: #{env.attributes[:title]},
-          uploaded_files_count: #{env.attributes[:uploaded_files].count},
-          object_version: #{env.attributes[:object_version]},
-          significant_fields_changed: #{significant_fields_changed(env)}
-        }"
+      def perform_object_versioning(env)
+        attach_object_version_changed_property(env)
+        increment_object_version(env) if should_increment_object_version?(env)
       end
 
-      def significant_fields_changed(env)
+      def attach_object_version_changed_property(env)
+        class << env
+          attr_accessor :object_version_changed
+          alias_method :object_version_changed?, :object_version_changed
+        end
+        env.object_version_changed = false
+      end
+
+      def should_increment_object_version?(env)
+        env.attributes[:object_version] == "" ||
+          (curation_concern_approved?(env.curation_concern) &&
+            significant_fields_changed?(env))
+      end
+
+      def curation_concern_approved?(curation_concern)
+        curation_concern.state == Vocab::FedoraResourceStatus.active
+      end
+
+      def significant_fields_changed?(env)
         env.curation_concern.title != env.attributes[:title] ||
           env.attributes[:uploaded_files].count.positive?
       end
 
       def increment_object_version(env)
+        if env.attributes[:object_version] == ""
+          env.attributes[:object_version] = "0"
+        end
+
         env.attributes[:object_version] =
             (env.attributes[:object_version].to_i + 1).to_s
+
+        env.object_version_changed = true
       end
     end
   end
