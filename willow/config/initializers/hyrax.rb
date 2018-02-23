131a6b8b4618b01c9e12a2f91b039207e2acf276
hwjisc <howard.williams@jisc.ac,uk>
2018-02-23 09:46:48 +0000

    curation_concern.rb and added actor to initializer

--- willow/config/initializers/hyrax.rb
+++ willow/config/initializers/hyrax.rb
@@ -238,3 +238,5 @@ Date::DATE_FORMATS[:default] = DEFAULT_DATE_FORMAT
 Qa::Authorities::Local.register_subauthority('subjects', 'Qa::Authorities::Local::TableBasedAuthority')
 Qa::Authorities::Local.register_subauthority('languages', 'Qa::Authorities::Local::TableBasedAuthority')
 Qa::Authorities::Local.register_subauthority('genres', 'Qa::Authorities::Local::TableBasedAuthority')
+
+Hyrax::CurationConcern.actor_factory.insert_after Hyrax::Actors::TransactionalRequest, Hyrax::Actors::RdssCdmObjectVersioningActor
