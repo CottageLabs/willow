c483f8a77ff20489f2a0711249762ee3919c9cfd
Daniel Berzon <daniel@ocasta.com>
2018-02-28 14:46:35 +0000

    Remove the `title_to_array` functionality from the actor stack.
    Note that this did not cause an error, as `Array([1]) == Array(1) == [1]`

--- willow/app/actors/hyrax/actors/rdss_cdm_actor.rb
+++ willow/app/actors/hyrax/actors/rdss_cdm_actor.rb
@@ -5,12 +5,10 @@ module Hyrax
     class RdssCdmActor < Hyrax::Actors::BaseActor
       def create(env)
         add_object_uuid(env)
-        title_to_array(env)
         super
       end
 
       def update(env)
-        title_to_array(env)
         super
       end
 
@@ -19,10 +17,6 @@ module Hyrax
           unless env.attributes.key?(:object_uuid)
             env.attributes[:object_uuid] = SecureRandom.uuid
           end
-        end
-
-        def title_to_array(env)
-          env.attributes[:title] = Array(env.attributes[:title]) if env.attributes[:title]
         end
     end
   end
