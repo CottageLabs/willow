ecf677e17c1e31f3c825d9ef38c2b0451e0210f0
Daniel Berzon <daniel@ocasta.com>
2018-03-01 13:54:43 +0000

    Add comment to the actor update method

--- willow/app/actors/hyrax/actors/rdss_cdm_actor.rb
+++ willow/app/actors/hyrax/actors/rdss_cdm_actor.rb
@@ -8,6 +8,11 @@ module Hyrax
         super
       end
 
+      # TODO Remove this method
+      # DMVB 2018-03-01 We should in theory be able to remove this overridden update 
+      # method completely. However, without it I have seen updates fail and return to the
+      # edit page. There were no errors in the model and it's as if the super call was returning falsey
+      # value. With the method below in place edits have always worked.
       def update(env)
         super
       end
