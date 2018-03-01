4c6ac728d5a94eb413d628f732fd16ff170e98ad
Paul McKibbin <pmckibbin@gmail.com>
2018-03-01 16:19:14 +0000

    Pull up to 2.1.0-beta1 of hyrax

--- willow/spec/rails_helper.rb
+++ willow/spec/rails_helper.rb
@@ -25,7 +25,7 @@ require 'devise'
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-# Checks for pending migration and applies them before tests are run.
+# Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
