020cf8ec21c0839df1952f76198fc8e5ecdd8a57
Daniel Berzon <daniel@ocasta.com>
2018-03-02 11:43:21 +0000

    Adding mail to the show page, and changing object_people format to a table of values

--- willow/app/models/cdm/json/object_person.rb
+++ willow/app/models/cdm/json/object_person.rb
@@ -1,11 +1,12 @@
 module Cdm
   module Json
     class ObjectPerson < ::Cdm::Json::ModelBase
-      attr_reader :honorific_prefix, :given_name, :family_name, :roles
+      attr_reader :honorific_prefix, :given_name, :family_name, :mail, :roles
       def initialize(values={}, converter=::Cdm::Json::ObjectPersonRole)
         @honorific_prefix=values['honorific_prefix']
         @given_name=values['given_name']
         @family_name=values['family_name']
+        @mail=values['mail']
         @roles=values['object_person_roles'].map {|x| converter.new(x) }
         super(values)
       end
