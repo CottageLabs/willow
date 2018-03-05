e0f3e97547a40d85a3278d1b5c42170bcc1cfe77
Paul McKibbin <pmckibbin@gmail.com>
2018-03-05 09:14:22 +0000

    Reinstated spec_helper without active_fedora/noid which has now been replaced.

--- willow/spec/spec_helper.rb
+++ willow/spec/spec_helper.rb
@@ -1,24 +1,17 @@
-# require 'rails_helper'
-# require 'active_fedora/noid/rspec'
-# require 'wisper/rspec/matchers'
-#
-#
-# RSpec.configure do |config|
-#   config.expect_with :rspec do |expectations|
-#     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-#   end
-#
-#   config.mock_with :rspec do |mocks|
-#     mocks.verify_partial_doubles = true
-#   end
-#
-#   config.shared_context_metadata_behavior = :apply_to_host_groups
-#
-#   # Excepton for deleting database-backed minter
-#   include ActiveFedora::Noid::RSpec
-#
-#   config.before(:suite) { disable_production_minter! }
-#   config.after(:suite)  { enable_production_minter! }
-#
-#   config.include(Wisper::RSpec::BroadcastMatcher)
-# end
+require 'rails_helper'
+require 'wisper/rspec/matchers'
+
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.include(Wisper::RSpec::BroadcastMatcher)
+end
