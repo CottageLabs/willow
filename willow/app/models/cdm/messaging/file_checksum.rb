bf26995cda1e801e8f9e2aeb432532a3f5a63093
Paul McKibbin <pmckibbin@gmail.com>
2018-03-08 14:48:19 +0000

    Make file checksum work as an array of hashes value rather than bare hash values.

--- willow/app/models/cdm/messaging/file_checksum.rb
+++ willow/app/models/cdm/messaging/file_checksum.rb
@@ -1,12 +1,12 @@
 module Cdm
   module Messaging
     class FileChecksum < MessageMapper
-      def hash_value(message_mapper, object)
-        {
+      def array_value(message_mapper, object)
+        [{
           checksumUuid: object.checksum_uuid,
           checksumType: Enumerations::ChecksumType.md5,
           checksumValue: object.original_file.original_checksum
-        }
+        }]
       end
     end
   end
