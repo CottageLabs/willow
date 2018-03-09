a07193c60fca937d89bf731f271c0eaab03a3543
howard-jisc <howard.williams@jisc.ac.uk>
2018-03-09 09:57:02 +0000

    tests for related identifiers update during object versioning

--- willow/spec/actors/hyrax/actors/rdss_cdm_object_versioning_actor_spec.rb
+++ willow/spec/actors/hyrax/actors/rdss_cdm_object_versioning_actor_spec.rb
@@ -6,6 +6,22 @@ RSpec.describe Hyrax::Actors::RdssCdmObjectVersioningActor do
   let(:depositor) { create(:user) }
   let(:active_state) {Vocab::FedoraResourceStatus.active}
   let(:inactive_state) {Vocab::FedoraResourceStatus.inactive}
+  let(:original_object_uuid) { SecureRandom.uuid }
+  let(:approved_rdss_cdm) { 
+    create(:rdss_cdm, 
+      title: ["test title"], 
+      object_version: "1", 
+      object_uuid: original_object_uuid, 
+      state: active_state) 
+    }
+
+  let(:unapproved_rdss_cdm) { 
+      create(:rdss_cdm, 
+        title: ["test title"], 
+        object_version: "1", 
+        object_uuid: original_object_uuid, 
+        state: inactive_state) 
+      }
 
   subject(:middleware) do
     stack = ActionDispatch::MiddlewareStack.new.tap do |middleware|
@@ -29,62 +45,80 @@ RSpec.describe Hyrax::Actors::RdssCdmObjectVersioningActor do
   end
 
   describe "minor update" do
-    let(:original_uuid) { SecureRandom.uuid }
-    let(:attributes) { {:object_version => "1", :title => ["test title"]} }
-    let(:rdss_cdm) { create(:rdss_cdm, title: ["test title"], object_version: "1", object_uuid: original_uuid, state: active_state) }
-    let(:env) { Hyrax::Actors::Environment.new(rdss_cdm, ability, attributes) }
+    let(:attributes) { {:object_version => "1", :title => ["test title"]} }    
+    let(:env) { Hyrax::Actors::Environment.new(approved_rdss_cdm, ability, attributes) }
     
     it 'object version increments to 2' do
       expect { middleware.update(env) }.to change { env.attributes[:object_version] }.to "2"
     end
 
     it "object related identifiers change to include previous object_uuid as is_new_version_of" do
-      middleware.update(env)
-
-      last_recorded_object_uuid = last_recorded_object_uuid(env)
-      expect(last_recorded_object_uuid).to eq(original_uuid)
-
-      last_recorded_relation_type = last_recorded_relation_type(env)
-      expect(last_recorded_relation_type).to eq('is_new_version_of')
+      object_uuid_recorded_in_related_identifiers_as_is_new_version_of?(env)
     end
-  end
-
-  def last_recorded_relation_type env
-    env.attributes[:object_related_identifiers_attributes][last_recorded_identifier_index(env)]['relation_type']
-  end
-
-  def last_recorded_object_uuid env
-    env.attributes[:object_related_identifiers_attributes][last_recorded_identifier_index(env)]['identifier_attributes']['identifier_value'] 
-  end
-
-  def last_recorded_identifier_index env            
-    (env.attributes[:object_related_identifiers_attributes].values.count-1).to_s
   end
 
   describe "major update to title" do
     let(:attributes) { {:object_version => "1", :title => ["another test title"]} }
-    let(:rdss_cdm) { create(:rdss_cdm, title: ["test title"], object_version: "1", state: active_state) }
-    let(:env) { Hyrax::Actors::Environment.new(rdss_cdm, ability, attributes) }
+    let(:env) { Hyrax::Actors::Environment.new(approved_rdss_cdm, ability, attributes) }
+
     it 'object version increments to 2' do
       expect { middleware.update(env) }.to change { env.attributes[:object_version] }.to "2"
+    end
+
+    it "object related identifiers change to include previous object_uuid as is_new_version_of" do
+      object_uuid_recorded_in_related_identifiers_as_is_new_version_of?(env)
     end
   end
 
   describe "major update to uploaded files" do
     let(:attributes) { {:object_version => "1", :title => ["test title"], :uploaded_files => [1,2]} }
-    let(:rdss_cdm) { create(:rdss_cdm, title: ["test title"], object_version: "1", state: active_state) }
-    let(:env) { Hyrax::Actors::Environment.new(rdss_cdm, ability, attributes) }
+    let(:env) { Hyrax::Actors::Environment.new(approved_rdss_cdm, ability, attributes) }
+
     it 'object version increments to 2' do
       expect { middleware.update(env) }.to change { env.attributes[:object_version] }.to "2"
+    end
+
+    it "object related identifiers change to include previous object_uuid as is_new_version_of" do
+      object_uuid_recorded_in_related_identifiers_as_is_new_version_of?(env)
     end
   end
 
   describe "major unpublished update" do
     let(:attributes) { {:object_version => "1", :title => ["another test title"]} }
-    let(:rdss_cdm) { create(:rdss_cdm, title: ["test title"], object_version: "1", state: inactive_state) }
-    let(:env) { Hyrax::Actors::Environment.new(rdss_cdm, ability, attributes) }
+    let(:env) { Hyrax::Actors::Environment.new(unapproved_rdss_cdm, ability, attributes) }
+
     it 'object version increments to 2' do
       expect { middleware.update(env) }.not_to change { env.attributes[:object_version] }
     end
+
+    it "object related identifiers change to not include previous object_uuid as is_new_version_of" do
+      !object_uuid_recorded_in_related_identifiers_as_is_new_version_of?(env)
+    end
+  end
+
+  def object_uuid_recorded_in_related_identifiers_as_is_new_version_of? env
+    middleware.update(env)
+
+    unless env.attributes[:object_related_identifiers_attributes].nil?
+      last_recorded_related_identifier_value = last_recorded_related_identifier_value(env)
+      expect(last_recorded_related_identifier_value).to eq(original_object_uuid)
+  
+      last_recorded_related_identifier_relation_type = last_recorded_related_identifier_relation_type(env)
+      expect(last_recorded_related_identifier_relation_type).to eq('is_new_version_of')
+    else
+      false
+    end
+  end
+
+  def last_recorded_related_identifier_relation_type env
+    env.attributes[:object_related_identifiers_attributes][last_recorded_related_identifier_index(env)]['relation_type']
+  end
+
+  def last_recorded_related_identifier_value env
+    env.attributes[:object_related_identifiers_attributes][last_recorded_related_identifier_index(env)]['identifier_attributes']['identifier_value'] 
+  end
+
+  def last_recorded_related_identifier_index env            
+    (env.attributes[:object_related_identifiers_attributes].values.count-1).to_s 
   end
 end
