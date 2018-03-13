024fdfc3fc593adc4f6dd72f4f3ac81f2bca85b1
Paul McKibbin <pmckibbin@gmail.com>
2018-03-13 08:51:26 +0000

    More defaults for situations where things aren't set

---
+++ willow/app/models/cdm/messaging/access_statement.rb
@@ -0,0 +1,12 @@
+# Endpoint for access message element. For the CDM model this maps to has_many :accesses, so the message
+# :access translates to the attribute :accesses in the CDM
+
+module Cdm
+  module Messaging
+    class AccessStatement < MessageMapper
+      def value(object, _)
+        object.access_statement || Enumerations::AccessType.send(object.access_type)
+      end
+    end
+  end
+end
